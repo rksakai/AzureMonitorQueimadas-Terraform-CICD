b65b4af74b4a37fdb14f58e38abf482129563776
Rodrigo <profrodrigo.sakai@fiap.com.br>
2026-08-27 22:39:31 +0000

    Revert "Ajustes"
    This reverts commit 9ccdf6f6b2ddce02135db4541e4a02b09e75113c.

--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "postgres_fqdn" {
-  value = azurerm_postgresql_flexible_server.postgres.fqdn
-}
-
-output "function_app_url" {
-  value = "https://${azurerm_linux_function_app.func.default_hostname}"
-}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,31 +1,22 @@
 variable "location" {
   default = "brazilsouth"
 }
-
 variable "resource_group_name" {
   default = "rg-monitor-queimadas"
 }
-
-variable "postgres_admin_user" {
+variable "mysql_admin_user" {
   default = "adminuser"
 }
-
-variable "postgres_admin_password" {
+variable "mysql_admin_password" {
   type      = string
   sensitive = true
 }
-
-variable "postgres_db_name" {
+variable "sql_db_name" {
   default = "db_queimadas"
 }
-
 variable "function_app_name" {
   default = "func-queimadas"
 }
-
 variable "storage_account_name" {
   default = "stqueimadasfunc"
 }
-
-# As variaveis acr_name, aci_name e webapp_image_tag serao
-# adicionadas neste mesmo arquivo no Dia 2.
